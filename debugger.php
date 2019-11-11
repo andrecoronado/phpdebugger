f0acabd4ab33a73194414ec4e0c978669e62767e
Andre Coronado <54828512+andrecoronado@users.noreply.github.com>
2019-11-10 21:00:34 -0500

    Update -> simplification
    - Removed function Close();
    - Include save file in a send function;
    - Include function clear();

--- debugger.php
+++ debugger.php
@@ -2,18 +2,18 @@
 
 class Debugger {
         private $pathFile; //path and file where the JSON will be save
-        private $past; //true or false | true -> keep and record the history | false -> record only the last time that it runned
+        private $hist; //true or false | true -> keep and record the history | false -> record only the last time that it runned
         private $jsonDataFinal= array(); // array with date from all compilation
         private $jsonDataInitial= array(); // array with data only in this compilation
         private $n; //count how many times did it run, if $past = true
         
 //exemple: $debugger= new Debugger('json/phpDebugger.json',true);
-        public function __construct($path, $past) {
+        public function __construct($path, $hist) {
             // 1- the path file is record    
             $this->pathFile=$path;
-            $this->past=$past;
+            $this->hist=$hist;
 
-            if($this->past)
+            if($this->hist)
             {
                 // 2- the old file is open and convert from json to array, if $past=true
                 $oldFile=json_decode(file_get_contents($this->pathFile));
@@ -37,13 +37,10 @@ class Debugger {
 //exemple: $debugger->send('Name variable or array',$array);
         public function send($text,$data){
             // 5- the data is record in a initial array
-            $this->jsonDataInitial+=[$text => $data];   
-        }
+            $this->jsonDataInitial+=[$text => $data];  
 
-//exemple: $debugger->close();
-        public function close(){
             // 6- the data is record in a final array
-            if($this->past)
+            if($this->hist)
             {
                 $n=$this->n;
                 $this->jsonDataFinal+=['p-'.$n => $this->jsonDataInitial];   
@@ -54,6 +51,13 @@ class Debugger {
             }
 
             // 7- the json is save in a file that was indicate in the first step
-            file_put_contents($this->pathFile, json_encode($this->jsonDataFinal));
+            file_put_contents($this->pathFile, json_encode($this->jsonDataFinal));                
+        }
+        
+        public function clear(){
+            $this->jsonDataInitial=['-- --'];
+            $this->jsonDataFinal=[$this->jsonDataInitial];
+            file_put_contents($this->pathFile, json_encode($this->jsonDataFinal));    
+                
         }
     }
